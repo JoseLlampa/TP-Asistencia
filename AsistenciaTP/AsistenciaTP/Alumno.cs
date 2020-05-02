ca9d2e6a47032c4b781d9025c0e8b90cf6fc43d7
pablo9306 <62298647+pablo9306@users.noreply.github.com>
2020-05-02 17:52:05 -0300

    V1

---
+++ AsistenciaTP/AsistenciaTP/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsistenciaTP
+{
+    public class Alumno
+    {
+        private int _registro;
+        private List<Falta> _faltas;
+        private double _ausencia;
+
+        public int Registro { get => _registro; set => _registro = value; }
+        public List<Falta> Faltas { get => _faltas; set => _faltas = value; }
+        public double Ausencia { get => _ausencia; set => _ausencia = value; }
+
+        
+    }
+}
---
+++ AsistenciaTP/AsistenciaTP/Curso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsistenciaTP
+{
+    public class Curso
+    {
+        private int _año;
+        private int _division;
+        private string _turno;
+
+        public int Año { get => _año; set => _año = value; }
+        public int Division { get => _division; set => _division = value; }
+        public string Turno { get => _turno; set => _turno = value; }
+    }
+}
